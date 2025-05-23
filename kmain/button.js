c21ebc0614507c7130729014c50404374fa4cb48
yamagata-hanbai <hide@yamagata-hanbai.co.jp>
2025-05-23 16:39:02 +0900

    KOZO250523

---
+++ kmain/button.js
@@ -0,0 +1,56 @@
+function jump09(){
+	parent.frame1.location.href = "../2009/2009.html";
+	parent.frame2.location.href = "../year/button09.html";
+}
+function jump10(){
+	parent.frame1.location.href = "../2010/2010.html";
+	parent.frame2.location.href = "../year/button10.html";
+}
+function jump11(){
+	parent.frame1.location.href = "../2011/2011.html";
+	parent.frame2.location.href = "../year/button11.html";
+}
+function jump12(){
+	parent.frame1.location.href = "../2012/2012.html";
+	parent.frame2.location.href = "../year/button12.html";
+}
+function jump13(){
+	parent.frame1.location.href = "../2013/2013.html";
+	parent.frame2.location.href = "../year/button13.html";
+}
+function jump14(){
+	parent.frame1.location.href = "../2014/2014.html";
+	parent.frame2.location.href = "../year/button14.html";
+}
+function jump15(){
+	parent.frame1.location.href = "../2015/2015.html";
+	parent.frame2.location.href = "../year/button15.html";
+}
+function jump16(){
+	parent.frame1.location.href = "../2016/2016.html";
+	parent.frame2.location.href = "../year/button16.html";
+}
+function jump17(){
+	parent.frame1.location.href = "../2017/2017.html";
+	parent.frame2.location.href = "../year/button17.html";
+}
+function jump18(){
+	parent.frame1.location.href = "../2018/2018.html";
+	parent.frame2.location.href = "../year/button18.html";
+}
+function jump19(){
+	parent.frame1.location.href = "../2019/2019.html";
+	parent.frame2.location.href = "../year/button19.html";
+}
+function jump20(){
+	parent.frame1.location.href = "../2020/2020.html";
+	parent.frame2.location.href = "../year/button20.html";
+}
+function jump21(){
+	parent.frame1.location.href = "../2021/2021.html";
+	parent.frame2.location.href = "../year/button21.html";
+}
+function jump22(){
+	parent.frame1.location.href = "../2022/2022.html";
+	parent.frame2.location.href = "../year/button22.html";
+}
